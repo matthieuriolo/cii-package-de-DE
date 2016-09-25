ec14e6d0bc4b165cae007a851a244e84d9c5ebb5
Matthieu Riolo <matthieu.riolo@googlemail.com>
2016-09-25 19:29:25 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'type' => 'message',
+    'name' => 'cii',
+    'messageType' => 'package',
+    'language' => 'de-De',
+    'version' => '0.1',
+    'description' => 'The german Cii package translation',
+    'created' => '2016-08-26 02:26:52',
+    'author' => [
+        'name' => 'Matthieu Riolo',
+        'email' => 'matthieu.riolo@gmail.com',
+        'website' => 'http://www.ocsource.ch',
+    ],
+];
